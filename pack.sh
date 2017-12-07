ee616ed801cff6627cd5d91324a52e7ab64644a9
nikonikolov <niko.b.nikolov@gmail.com>
2017-12-07 02:27:38 +0100

    added DART, nlopt, cudnn, fixed pcl

--- pack.sh
+++ pack.sh
@@ -10,10 +10,10 @@ if [ $# -eq 0 ]; then
     echo "Error: No arguments provided. You must provide a package name"
     exit
 else
-    echo "Ignore ls output errors"
+    echo "NOTE: IGNORE THE ls OUTPUT ERRORS"
     PKG=${1%/}
     echo "$PKG"
-    PATH_SRC=`ls $PKG/$PKG-*.tar.gz || ls $PKG/$PKG-*.tar.bz2` 
+    PATH_SRC=`ls $PKG/$PKG-*.tar.gz || ls $PKG/$PKG-*.tar.bz2 || ls $PKG/${PKG,,}-*.tar.gz || ls $PKG/${PKG,,}-*.tar.bz2`
     SRC=$(basename "$PATH_SRC")
     mv $PKG/$SRC . 
     tar -cvf $OUTPUT_DIR/$PKG.tar.gz $PKG/
